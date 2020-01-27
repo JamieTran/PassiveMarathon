50e6d477f090e8a6641afd74fb5d9415d7559a5d
Adam Tunkiewicz <adastunkiewicz@gmail.com>
2020-01-27 16:35:12 -0500

    :sparkles: Add constants file

---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+library Constants;
+import 'package:flutter/material.dart';
+
+const Color pastel_green = Color(0xFF469DA0);
+const Color pastel_yellow = Color(0xFFEBE280);
+const Color pastel_red = Color(0xFFBE5968);
+const Color pastel_black = Color(0xFF2F343C);
+const Color pastel_darkGreen = Color(0xFF97BE54);
+
+const Color nautical_tan = Color(0xFFC9BB89);
+const Color nautical_yellow = Color(0xFFF5EF9F);
+const Color nautical_lightBlue = Color(0xFF445680);
+const Color nautical_mediumBlue= Color(0xFF2E3C61);
+const Color nautical_darkBlue = Color(0xFF15264A);
+
+const Color salmon_black = Color(0xFF3E454C);
+const Color salmon_darkBlue = Color(0xFF2185C5);
+const Color salmon_lightBlue = Color(0xFF7ECEFD);
+const Color salmon_eggshell = Color(0xFFFFF6E5);
+const Color salmon_orange = Color(0xFFFF7F66);
+
+const Color bright_white = Color(0xfff5f0e7);
+const Color bright_red = Color(0xffff461e);
+const Color bright_blue= Color(0xff3cbee3);
+const Color bright_yellow = Color(0xfffdb321);
+const Color bright_purple = Color(0xffa862ac);
+
+
