c824c2d37f77f8bf82d19de40d23987669323d28
Adam Tunkiewicz <adastunkiewicz@gmail.com>
2020-01-27 16:40:43 -0500

    :sparkles: Add new individual files for screens

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'marathon_groups.dart';
+import 'friend_management.dart';
+import 'constants.dart' as Constants;
 
 // Stateful widgets are used when you need to update the screen
 // with data constantly, this works for passive marathon
@@ -12,13 +15,114 @@ class HomePageState extends State<HomePage>{
   Widget build(BuildContext context){
     return new Scaffold(
       appBar: new AppBar(
-        title:new Text("Passive Marathon")
+        title:new Text("Passive Marathon", style: TextStyle(color: Constants.salmon_eggshell)),
+        backgroundColor: Constants.salmon_black,
       ),
-      body: new Container(
-        child: new Center(
-          child: new Text("Passive Marathon"),
-        ),
-      )
+      backgroundColor: Constants.bright_white,
+      body: new GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Constants.bright_red,
+            child: new FlatButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MarathonGroups()),
+                  );              
+                },
+                child: Column( // Replace with a Row for horizontal icon + text
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Icon(Icons.directions_run, size: 100.0, color: Constants.bright_white),
+                    Text("Marathons", style: TextStyle(color: Constants.bright_white))
+                  ],
+                ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Constants.bright_blue,
+            child: new FlatButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FriendsManagement()),
+                  );  
+              },
+                child: Column( // Replace with a Row for horizontal icon + text
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Icon(Icons.group, size: 100.0, color: Constants.bright_white),
+                    Text("Friends", style: TextStyle(color: Constants.bright_white))
+                  ],
+                ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Constants.bright_purple,
+            child: new FlatButton(
+              onPressed: () {
+                /*...*/
+              },
+              child: Column( // Replace with a Row for horizontal icon + text
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(Icons.account_circle, size: 100.0, color: Constants.bright_white),
+                  Text("Profile", style: TextStyle(color: Constants.bright_white))
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Constants.bright_yellow,
+            child: new FlatButton(
+              onPressed: () {
+                /*...*/
+              },
+              child: Column( // Replace with a Row for horizontal icon + text
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(Icons.settings, size: 100.0, color: Constants.bright_white),
+                  Text("Settings", style: TextStyle(color: Constants.bright_white))
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Constants.bright_white,
+            child: new FlatButton(
+              onPressed: () {
+                /*...*/
+              },
+              child: Text(
+                "Extra Option 1",
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Constants.bright_white,
+            child: new FlatButton(
+              onPressed: () {
+                /*...*/
+              },
+              child: Text(
+                "Extra Option 2",
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/marathon_groups.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'constants.dart' as Constants;
+
+// Stateful widgets are used when you need to update the screen
+// with data constantly, this works for passive marathon
+class MarathonGroups extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Marathon Groups"),
+        backgroundColor: Constants.bright_red,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
