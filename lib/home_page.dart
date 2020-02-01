642209e974f18096b4323a6cc012ac61a23d3dbf
Adam Tunkiewicz <adastunkiewicz@gmail.com>
2020-02-01 16:04:34 -0500

    :art: Swap button code to use new generator class

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:passive_marathon/profile_page.dart';
+import 'package:passive_marathon/settings_page.dart';
 import 'marathon_groups.dart';
 import 'friend_management.dart';
+import 'nav_button.dart';
 import 'constants.dart' as Constants;
 
 // Stateful widgets are used when you need to update the screen
@@ -27,100 +30,10 @@ class HomePageState extends State<HomePage>{
         crossAxisCount: 2,
         
         children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Constants.bright_red,
-            child: new FlatButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MarathonGroups()),
-                  );              
-                },
-                child: Column( // Replace with a Row for horizontal icon + text
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Icon(Icons.directions_run, size: 100.0, color: Constants.bright_white),
-                    Text("Marathons", style: TextStyle(color: Constants.bright_white))
-                  ],
-                ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Constants.bright_blue,
-            child: new FlatButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FriendsManagement()),
-                  );  
-              },
-                child: Column( // Replace with a Row for horizontal icon + text
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Icon(Icons.group, size: 100.0, color: Constants.bright_white),
-                    Text("Friends", style: TextStyle(color: Constants.bright_white))
-                  ],
-                ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Constants.bright_purple,
-            child: new FlatButton(
-              onPressed: () {
-                /*...*/
-              },
-              child: Column( // Replace with a Row for horizontal icon + text
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Icon(Icons.account_circle, size: 100.0, color: Constants.bright_white),
-                  Text("Profile", style: TextStyle(color: Constants.bright_white))
-                ],
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Constants.bright_yellow,
-            child: new FlatButton(
-              onPressed: () {
-                /*...*/
-              },
-              child: Column( // Replace with a Row for horizontal icon + text
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Icon(Icons.settings, size: 100.0, color: Constants.bright_white),
-                  Text("Settings", style: TextStyle(color: Constants.bright_white))
-                ],
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Constants.bright_white,
-            child: new FlatButton(
-              onPressed: () {
-                /*...*/
-              },
-              child: Text(
-                "Extra Option 1",
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Constants.bright_white,
-            child: new FlatButton(
-              onPressed: () {
-                /*...*/
-              },
-              child: Text(
-                "Extra Option 2",
-              ),
-            ),
-          ),
+          new NavButton(Constants.bright_red, Constants.bright_white, Constants.bright_white, "Marathons", Icons.directions_run, MaterialPageRoute(builder: (context) => MarathonGroups())),
+          new NavButton(Constants.bright_blue, Constants.bright_white, Constants.bright_white, "Friends", Icons.group,MaterialPageRoute(builder: (context) => FriendsManagement())),
+          new NavButton(Constants.bright_purple, Constants.bright_white, Constants.bright_white, "Profile", Icons.account_circle ,MaterialPageRoute(builder: (context) => ProfilePage())),
+          new NavButton(Constants.bright_yellow, Constants.bright_white, Constants.bright_white, "Settings", Icons.settings ,MaterialPageRoute(builder: (context) => SettingsPage())),
         ],
       ),
     );
