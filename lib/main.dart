7dd1cc5296b3098eb8290cb8b26ccf7f117623f4
JamieTran <32204301+JamieTran@users.noreply.github.com>
2020-02-01 16:39:28 -0500

    :bug: Fix issue directory

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 import 'package:flutter/material.dart';
-import './landing_pages.dart';
+import './landing_pages/landing_pages.dart';
 
 void main() {
   runApp(new MyApp());
