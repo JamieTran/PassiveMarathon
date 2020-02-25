e04fee48f97cf52c450c5cca664ba4fbf15603ca
Adam Tunkiewicz <adastunkiewicz@gmail.com>
2020-02-24 19:05:52 -0500

    :sparkles: Added new functions to get firebase data

--- lib/db_management.dart
+++ lib/db_management.dart
@@ -1,25 +1,54 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 
 class DatabaseManagement{
 
   final databaseReference = Firestore.instance;
 
   void createUser(id) async {
+    print('Add user');
   await databaseReference.collection("users")
-      .document("test")
-      .setData({
-        'userID': '1',
-        'first_name':'Yeet',
-        'last_name':'Boi',
-        'dob':'',
-        'token':'',
-      });
+    .add({
+      'userID': '6',
+      'name':'Mateus Gurgel',
+      'searchKey':'M',
+      'dob':'',
+      'token':'',
+      'friends': [''],
+    });
   }
 
   void createGroup(groupName) async {
   await groupName.collection("groups")
       .document(groupName);
   }
-  
+
+  queryUsers(String username) {
+    return databaseReference.collection('users')
+    .where('searchKey', isEqualTo: username.substring(0,1).toUpperCase())
+    .getDocuments();
+  }
+
+  addFriend(addedFriend) { // Add to Adam Array because he is lonely
+    DocumentReference array =  databaseReference.collection('users').document('NPne34FyhahEnMXHaYh5');
+
+    array.get().then((datasnapshot) {
+    if (datasnapshot.exists) {
+      List<dynamic> friendArray = datasnapshot.data['friends'].toList();
+      friendArray.add(addedFriend);
+      databaseReference.collection('users').document('NPne34FyhahEnMXHaYh5').updateData({"friends": FieldValue.arrayUnion(friendArray)});
+      }
+    });
+  }
+
+  getFriends() {
+    DocumentReference array =  databaseReference.collection('users').document('NPne34FyhahEnMXHaYh5');
+    List<dynamic> friendArray = List<dynamic>();
+    array.get().then((datasnapshot) {
+    if (datasnapshot.exists) {
+      friendArray = datasnapshot.data['friends'].toList();
+      print("ARRAY---> " + friendArray.toString());
+    }
+    return friendArray;
+  });
+  }
 }
