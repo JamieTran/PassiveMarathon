3f4d3ea45550e2e3ec050524f5c98c9e2e6a2615
JamieTran <32204301+JamieTran@users.noreply.github.com>
2020-03-10 11:46:43 -0400

    :bug: Fix keyboard cutting off group window

--- lib/group_pages/create_group.dart
+++ lib/group_pages/create_group.dart
@@ -12,34 +12,40 @@ class CreateGroup extends StatefulWidget{
 class _CreateGroupState extends State{
   @override
   Widget build(BuildContext context) {
-          return AlertDialog(
-            title: Text('Add a new Marathon'),
-            content: Column( 
-              children: <Widget> [
-                TextField(
-                  decoration: InputDecoration(hintText: "Name of Group"),
-                ),
-                new DistanceSlider(),
-                TextField(
-                  decoration: InputDecoration(hintText: "Name of Group"),
-                ),
-              ],
-            ),
-            actions: <Widget>[
-              new FlatButton(
-                child: new Text('CANCEL'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
+    return AlertDialog(
+      title: Text('Add a new Marathon'),
+      content: SingleChildScrollView(
+          child: Column( 
+            children: <Widget> [
+              TextField(
+                decoration: InputDecoration(hintText: "Name of Group"),
               ),
-              new FlatButton(
-                child: new Text('ADD'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
+              Padding(
+                padding: EdgeInsets.only(top: 16.0),
+                child: Text("Distance"),
+              ),
+              DistanceSlider(),
+              TextField(
+                decoration: InputDecoration(hintText: "Name of Group"),
+              ),
             ],
-          );
+          ),
+        ),
+      actions: <Widget>[
+        new FlatButton(
+          child: new Text('CANCEL'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        new FlatButton(
+          child: new Text('ADD'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+    );
   }
 
 }
