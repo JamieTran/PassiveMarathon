6d5aba9d5fdc1d1c9941bc0ad544312a28a7b8c3
JamieTran <32204301+JamieTran@users.noreply.github.com>
2020-03-15 20:04:52 -0400

    :construction: Getting step requests to work

--- lib/profile_pages/profile_page.dart
+++ lib/profile_pages/profile_page.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import '../constants.dart' as Constants;
 
 // Stateful widgets are used when you need to update the screen
 // with data constantly, this works for passive marathon
-class MarathonGroups extends StatelessWidget {
+class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,12 +41,25 @@ class MarathonGroups extends StatelessWidget {
   
   void choiceAction(String choice) {
     if (choice == Constants.create_group) {
-      print('Create Group page here');
+      fetchSteps();
     } else if (choice == Constants.add_group) {
       print('Add group page here');
     } else if (choice == Constants.leave_group) {
       print('Leave Group page here');
     }
+  }
+
+  Future<http.Response> fetchSteps() async {
+    final response = await http.get(
+    'https://marathon-web-api.herokuapp.com/user/1/steps',
+      // Send authorization headers to the backend.S
+      headers: {HttpHeaders.authorizationHeader: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIzIiwiaXNzIjoiTWFyYXRob24ifQ.f6FWsbhF1IabP0ItC2nNCoLxlpFuHBne8kYw_FrVRAY"},
+    );
+    print(response);
+    final responseJson = json.decode(response.body);
+
+    print(responseJson);
+
   }
 
 }
