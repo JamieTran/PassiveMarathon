4d31ee451170a26181a0fd95b25f91452481747d
JamieTran <32204301+JamieTran@users.noreply.github.com>
2020-03-15 21:27:38 -0400

    :sparkles: Requests for steps

--- lib/profile_pages/profile_page.dart
+++ lib/profile_pages/profile_page.dart
@@ -50,10 +50,11 @@ class ProfilePage extends StatelessWidget {
   }
 
   Future<http.Response> fetchSteps() async {
+    // TODO: make user & date dynamic for what is being requested
+    // TODO: also make authentication token private
     final response = await http.get(
-    'https://marathon-web-api.herokuapp.com/user/1/steps',
-      // Send authorization headers to the backend.S
-      headers: {HttpHeaders.authorizationHeader: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIzIiwiaXNzIjoiTWFyYXRob24ifQ.f6FWsbhF1IabP0ItC2nNCoLxlpFuHBne8kYw_FrVRAY"},
+    'https://marathon-web-api-staging.herokuapp.com/user/1/steps?date=2020-03-15',
+      headers: {HttpHeaders.authorizationHeader: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiI2IiwiaXNzIjoiTWFyYXRob24ifQ.PY_7DnSqhgvromUG8x7JSUJl2AdA8sR14vFcaz-vtMM"},
     );
     print(response);
     final responseJson = json.decode(response.body);
