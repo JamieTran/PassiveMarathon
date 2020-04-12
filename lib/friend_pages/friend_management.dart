9c3086b5fdc62007d0811d9cd3d458db7593bde4
Adam Tunkiewicz <adastunkiewicz@gmail.com>
2020-04-12 09:24:15 -0400

    :art: add streambuilder for live updates

--- lib/friend_pages/friend_management.dart
+++ lib/friend_pages/friend_management.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:passive_marathon/db_management.dart';
 import 'package:passive_marathon/friend_pages/friend_add_page.dart';
@@ -13,42 +14,6 @@ class FriendScreen extends StatefulWidget {
 }
 
 class FriendsManagement extends State<FriendScreen> {
-@override
-void initState() {
-  super.initState();
-  updateList();
-}
-
-//ist<dynamic> friendsArray = new List<dynamic>();
-
-//List<String> friendsList = new List<String>();
-Map<String, String> friendsList = new Map<String, String>();
-var friendArray =[];
-
-updateList()
-{
-  friendArray.clear();
-  friendsList.clear();
-  DatabaseManagement().getFriendsArray().get().then((datasnapshot) {
-    if (datasnapshot.exists) {
-      friendsList = Map.from(datasnapshot.data['friends']);
-      var list = friendsList.keys.toList();
-      print(friendsList.toString());
-      //setState(() {
-/*         if (friendsList.isNotEmpty)
-        {
-          friendArray.add(friendsList.keys);
-        }
-      }); */
-      for (int i=0;i<friendsList.length;i++){
-        setState((){
-          friendArray.add(list[i]);
-          });      
-        }
-      print("OUTSIDE FUNCTION ->"+friendArray.toString());
-    }
-  });
-}
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,28 +29,39 @@ updateList()
                await Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => FriendAdd()));
-                setState((){
-                  updateList();
-               }); 
               },
             ),
         ]),
       backgroundColor: Constants.bright_white,
       body:
-        ListView(
-          children: <Widget>[
-            SizedBox(height:10.0),
-            GridView.count(
-              padding: EdgeInsets.only(left:10.0, right:10.0),
-              crossAxisCount: 2,
-              crossAxisSpacing: 4.0,
-              mainAxisSpacing: 4.0,
-              primary:false,
-              shrinkWrap: true,
-              children: friendArray.map((element) {
-                return buildResultCard(element, element,null, context,Constants.remove_friend, updateList);
-              }).toList()),
-          ],
+        StreamBuilder<DocumentSnapshot>(
+          stream: Firestore.instance.collection('users').document(DatabaseManagement().getUserRef()).snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot)
+          {
+            if (snapshot.hasError) {
+              return Text ('Error: ${snapshot.error}');
+            }
+            switch (snapshot.connectionState)
+            {
+              case ConnectionState.waiting:
+                return Text ('Loading...');
+              default:
+                return ListView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  itemCount: snapshot.data['friends'].length,
+                  itemBuilder: (_, int index) {
+                    String key = snapshot.data['friends'].keys.elementAt(index);
+                      return Card(
+                                  child: ListTile(
+                                    onTap: null,    // REDIRECT TO USER PROFILE HERE, I didn't test it but it should work, I hope
+                                    leading: Icon(Icons.account_circle),
+                                    title: Text('$key'),
+                                  ),
+                    );
+                  }
+                );     
+            }
+          },
         )
     );
   }
