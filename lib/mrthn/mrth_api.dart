6d5801b738476d9c02881b6afef00de5fec70af2
JamieTran <32204301+JamieTran@users.noreply.github.com>
2020-04-19 17:18:08 -0400

    :sparkles: ProfilePage now updates with a call to mrthn to update distance

--- lib/mrthn/mrth_api.dart
+++ lib/mrthn/mrth_api.dart
@@ -1,21 +1,29 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../constants.dart' as Constants;
 
 
 class MrthnAPI {
-  static Future<http.Response> fetchSteps() async {
+  static Future<String> fetchDistance(String mrthnID) async {
     // TODO: make user & date dynamic for what is being requested
     // TODO: also make authentication token private
     final response = await http.get(
-    'https://api.mrthn.dev/user/' + Constants.user_id.toString() + '/distance?date=2020-03-30',
+    'https://api.mrthn.dev/user/' + mrthnID + '/distance/over-period?date=2020-02-13&period=30d&largestOnly=false',
       headers: {HttpHeaders.authorizationHeader: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxIiwiaXNzIjoiTWFyYXRob24ifQ.1iZiWav2Ya6-jvQLkzQzoOuGCzjBK56R8pAiqnkF2UU"},
     );
-    print(response);
-    final responseJson = json.decode(response.body);
+    return compute(parseValue, response.body);
+  }
 
-    print(responseJson);
+  static String parseValue(String responseBody) {
+    
+    var responseJson = json.decode(responseBody);
+    //print(responseBody);
+    var result = responseJson['result'];
+    //print(result[0]['value'].toString());
+    return result[0]['value'].toString();
+
 
   }
 
