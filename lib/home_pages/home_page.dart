8265ff30ade3ba63e38513690139e73d542c8711
JamieTran <32204301+JamieTran@users.noreply.github.com>
2020-04-29 23:02:53 -0400

    :art: Update formatting

--- lib/home_pages/home_page.dart
+++ lib/home_pages/home_page.dart
@@ -9,17 +9,18 @@ import '../constants.dart' as Constants;
 
 // Stateful widgets are used when you need to update the screen
 // with data constantly, this works for passive marathon
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   State createState() => new HomePageState();
 }
 
-class HomePageState extends State<HomePage>{
+class HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title:new Text("Passive Marathon", style: TextStyle(color: Constants.salmon_eggshell)),
+        title: new Text("Lazy Olympics",
+            style: TextStyle(color: Constants.salmon_eggshell)),
         backgroundColor: Constants.salmon_black,
       ),
       backgroundColor: Constants.bright_white,
@@ -29,12 +30,37 @@ class HomePageState extends State<HomePage>{
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
         crossAxisCount: 2,
-        
         children: <Widget>[
-          new NavButton(Constants.bright_red, Constants.bright_white, Constants.bright_white, "Marathons", Icons.directions_run, MaterialPageRoute(builder: (context) => GroupsScreen())),
-          new NavButton(Constants.bright_blue, Constants.bright_white, Constants.bright_white, "Friends", Icons.group,MaterialPageRoute(builder: (context) => FriendScreen())),
-          new NavButton(Constants.bright_purple, Constants.bright_white, Constants.bright_white, "Profile", Icons.account_circle ,MaterialPageRoute(builder: (context) => ProfilePage(Constants.dbManagement.getUserRef()))),
-          new NavButton(Constants.bright_yellow, Constants.bright_white, Constants.bright_white, "Invites", Icons.mail_outline ,MaterialPageRoute(builder: (context) => InviteScreen())),
+          new NavButton(
+              Constants.bright_red,
+              Constants.bright_white,
+              Constants.bright_white,
+              "Marathons",
+              Icons.directions_run,
+              MaterialPageRoute(builder: (context) => GroupsScreen())),
+          new NavButton(
+              Constants.bright_blue,
+              Constants.bright_white,
+              Constants.bright_white,
+              "Friends",
+              Icons.group,
+              MaterialPageRoute(builder: (context) => FriendScreen())),
+          new NavButton(
+              Constants.bright_purple,
+              Constants.bright_white,
+              Constants.bright_white,
+              "Profile",
+              Icons.account_circle,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      ProfilePage(Constants.dbManagement.getUserRef()))),
+          new NavButton(
+              Constants.bright_yellow,
+              Constants.bright_white,
+              Constants.bright_white,
+              "Invites",
+              Icons.mail_outline,
+              MaterialPageRoute(builder: (context) => InviteScreen())),
         ],
       ),
     );
